9c4c7079004b22adbf309ae20581dc797614f9fc
Robert Leung <rleung84@uwo.ca>
2023-09-07 22:26:37 -0400

    updates to sidebar layout

--- src/index.js
+++ src/index.js
@@ -1,43 +1,42 @@
 import "./style.css";
 
+function createIcon(symbol) {
+  let div = document.createElement("div");
+  div.classList.add("material-symbols-outlined");
+  div.innerHTML = symbol;
+  return div;
+}
+function createIconLabel(symbol, label) {
+  let div = document.createElement("div");
+  let divIcon = createIcon(symbol);
+  let divLabel = document.createElement("div");
+  divLabel.innerHTML = label;
+  div.append(divIcon, divLabel);
+  return div;
+}
+
 function menuBar() {
   let menuBar = document.createElement("div");
   menuBar.classList.add("header");
   let menuLeft = document.createElement("div");
   menuLeft.classList.add("menuLeft");
-  let menuIcon = document.createElement("div");
-  menuIcon.classList.add("material-symbols-outlined");
-  menuIcon.innerHTML = "menu";
+  let menuIcon = createIcon("menu");
   menuIcon.addEventListener("click", toggleSidePanel);
-  let homeIcon = document.createElement("div");
-  homeIcon.classList.add("material-symbols-outlined");
-  homeIcon.innerHTML = "home";
+  let homeIcon = createIcon("home");
   let searchBar = document.createElement("div");
   searchBar.classList.add("searchBar");
-  let searchIcon = document.createElement("div");
-  searchIcon.classList.add("material-symbols-outlined");
-  searchIcon.innerHTML = "search";
+  let searchIcon = createIcon("search");
   let searchInput = document.createElement("input");
   searchInput.placeholder = "Search";
   searchBar.append(searchIcon, searchInput);
   menuLeft.append(menuIcon, homeIcon, searchBar);
   let menuRight = document.createElement("div");
   menuRight.classList.add("menuRight");
-  let addIcon = document.createElement("div");
-  addIcon.classList.add("material-symbols-outlined");
-  addIcon.innerHTML = "add";
-  let statusIcon = document.createElement("div");
-  statusIcon.classList.add("material-symbols-outlined");
-  statusIcon.innerHTML = "clock_loader_40";
-  let helpIcon = document.createElement("div");
-  helpIcon.classList.add("material-symbols-outlined");
-  helpIcon.innerHTML = "help";
-  let notificationIcon = document.createElement("div");
-  notificationIcon.classList.add("material-symbols-outlined");
-  notificationIcon.innerHTML = "notifications";
-  let userIcon = document.createElement("div");
-  userIcon.classList.add("material-symbols-outlined");
-  userIcon.innerHTML = "hdr_auto";
+  let addIcon = createIcon("add");
+  let statusIcon = createIcon("clock_loader_40");
+  let helpIcon = createIcon("help");
+  let notificationIcon = createIcon("notifications");
+  let userIcon = createIcon("hdr_auto");
   menuRight.append(addIcon, statusIcon, helpIcon, notificationIcon, userIcon);
   menuBar.append(menuLeft, menuRight);
   return menuBar;
@@ -46,6 +45,10 @@ function menuBar() {
 function sidebar() {
   let sidebar = document.createElement("div");
   sidebar.classList.add("sidebar");
+  let inbox = createIconLabel("inbox", "Inbox");
+  let today = createIconLabel("today", "Today");
+  let upcoming = createIconLabel("calendar_month", "Upcoming");
+  sidebar.append(inbox, today, upcoming);
   return sidebar;
 }
 
